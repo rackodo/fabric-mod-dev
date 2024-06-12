77de323bd32c64bc69af2fe0895abda3c3e3e4db
Bash Elliott <bashelliott@gmail.com>
2024-06-12 21:35:40 +1000

    init

---
+++ src/main/java/com/rackodo/minesillies/MinesilliesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rackodo.minesillies;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinesilliesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
